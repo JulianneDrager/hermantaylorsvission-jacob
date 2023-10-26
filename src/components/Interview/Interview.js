66998f1b5e20127d45b61937558cf55fa141404a
Jacob G <jrgrant560@gmail.com>
2023-10-26 11:00:59 -0400

    started styling interview mobile view

--- src/components/Interview/Interview.js
+++ src/components/Interview/Interview.js
@@ -12,10 +12,11 @@ const Interview = () => {
     const matchedSm = useMediaQuery("(max-width: 600px)");
 
     // styles
-    const interviewCon = InterviewStyle.interviewCon;
+    const mainCon = InterviewStyle.mainCon;
     const imgBackground = InterviewStyle.imgBackground;
     const imgTopper = InterviewStyle.imgTopper;
     const contentCon = InterviewStyle.contentCon;
+    const imgSpeakerIcon = InterviewStyle.imgSpeakerIcon;
     const h1Style = InterviewStyle.h1Style;
     const videoCon = InterviewStyle.videoCon;
 
@@ -23,14 +24,15 @@ const Interview = () => {
         <>
             {/* mobile view */}
             {matchedSm &&
-                <main className={interviewCon}>
+                <main className={mainCon}>
                     <img className={imgBackground}/>
                     <img className={imgTopper}/>
                     <div className={contentCon}>
-                        <h1 className={h1Style}></h1>
-                        <h1 className={h1Style}></h1>
+                        <img className={imgSpeakerIcon}/>
+                        <h1 className={h1Style}><span>Life's Chapter 1</span></h1>
+                        <h1 className={h1Style}>Herman's Exclusive Interview</h1>
                         <div className={videoCon}>
-                            <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FDrSuccessMan%2Fvideos%2F5855939064510164%2F&show_text=false&width=560&t=0" width="560" height="314" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                            {/* <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FDrSuccessMan%2Fvideos%2F5855939064510164%2F&show_text=false&width=560&t=0" width="560" height="314" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe> */}
                         </div>
                     </div>
                 </main>
